60960337706efad4678047bdc5081f9e2aae361d
recuu <65301589+Lohonek@users.noreply.github.com>
2023-09-25 13:20:27 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ btn.addEventListener('click', oblicz)
 
 function oblicz(){
     const wynik = document.querySelector('#wynik')
+    const wpow = document.querySelector('#wpow')
 
     const wartoscx = document.querySelector('#wartoscx').value
     const wartoscy = document.querySelector('#wartoscy').value
     const pow = wartoscx * wartoscy;
     const cena = pow * 8;
 
-    wynik.innerHTML = cena + 'zl'
+    wpow.innerHTML = 'Powierzchnia calkowita scian: ' + pow + 'm2';
+    wynik.innerHTML = 'Koszt malowania: ' + cena + 'zl'
 
 }
